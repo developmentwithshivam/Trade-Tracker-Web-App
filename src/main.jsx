18f47cae7d4124c4411e5abce429fd5bb4432172
developmentwithshivam <developmentwithshivam@gmail.com>
2025-04-30 15:43:28 +0530

    checkBrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -4,19 +4,19 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { store } from "./redux/store.js";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router";
-// import { HashRouter } from "react-router-dom"; // ← use from 'react-router-dom'
+// import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router-dom"; // ← use from 'react-router-dom'
 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-    <BrowserRouter>
-    {/* <HashRouter> */}
+    {/* <BrowserRouter> */}
+    <HashRouter>
   <App />
-{/* </HashRouter> */}
+</HashRouter>
 
-    </BrowserRouter>
+    {/* </BrowserRouter> */}
     </Provider>
   </StrictMode>
 );
